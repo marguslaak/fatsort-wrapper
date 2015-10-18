608927add0f586a0b0057ad355d6a47d73ac3485
Margus Laak <margus.laak@redfunction.ee>
2015-10-18 13:41:44 +0300

    name changes add fatsort_wrapper command script

---
+++ fatsort_wrapper/__main__.py
@@ -0,0 +1,9 @@
+import sys
+import fatsort_wrapper
+
+
+def main(args=None):
+    fatsort_wrapper.sort_fats()
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='autosort-vfat',
+    name='fatsort-wrapper',
     version='0.1',
-    packages=[''],
+    packages=['fatsort_wrapper'],
     url='',
     license='MIT',
     author='Margus Laak',
     author_email='margus.laak@redfunction.ee',
-    description='Automatically sort and unmount all vfat filesystems'
+    description='Automatically sort and unmount all vfat filesystems',
+    entry_points = {
+              'console_scripts': [
+                  'fatsort_wrapper = fastsort_wrapper.__main__:main',
+              ],
+          },
 )
